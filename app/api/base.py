47f4a51d7c33b113c8d281291ab456ac992c4136
ramasbeinaty <ramafadi@hotmail.com>
2022-10-29 02:41:53 +0400

    v3

--- app/api/base.py
+++ app/api/base.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from app.api.controllers import ambassador, visitor
+
+from app.core.config import settings
 
 api_router = APIRouter()
 
 # root api endpoint
 @api_router.get("/")
 async def read_root():
-        return {"status": "alive"}
+        return RedirectResponse(settings.TARGET_URL)
 
 api_router.include_router(visitor.router, prefix="/links", tags="links")
 api_router.include_router(ambassador.router, prefix="/leaderboard", tags="leaderboard")
